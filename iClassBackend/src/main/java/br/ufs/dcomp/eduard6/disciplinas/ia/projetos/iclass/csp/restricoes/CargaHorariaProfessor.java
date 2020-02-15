05d19e709d78e719b42c00b5f49b798a5a1167a3
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2020-02-15 12:46:09 -0300

    Adicionada restrição que limita a quantidade de horários de um professor à sua carga horária semanal.

--- iClassBackend/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/projetos/iclass/csp/restricoes/CargaHorariaProfessor.java
+++ iClassBackend/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/projetos/iclass/csp/restricoes/CargaHorariaProfessor.java
@@ -1,11 +1,13 @@
 package br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.restricoes;
 
+import java.util.HashMap;
 import java.util.List;
 
 import aima.core.search.csp.Assignment;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.IClassCSP;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.variables.IClassDomainRepresentation;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.variables.TurmaVariable;
+import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.to.ProfessorTO;
 
 /**
  * A quantidades de aulas que um professor dá, deve menor ou igual que a carga
@@ -21,11 +23,30 @@ public class CargaHorariaProfessor extends IClassRestricaoBase {
 
 	@Override
 	public List<TurmaVariable> getScope() {
-		return null;
+		return getiClassCSP().getVariables();
 	}
 
 	@Override
 	public boolean isSatisfiedWith(Assignment<TurmaVariable, IClassDomainRepresentation> assignment) {
+		HashMap<ProfessorTO, Integer> professorCargaHoraria = new HashMap<>();
+		for (TurmaVariable turmaVariable : getScope()) {
+			IClassDomainRepresentation value = assignment.getValue(turmaVariable);
+			if (value != null) {
+				Integer profCargaSum = professorCargaHoraria.get(value.getProfessor());
+
+				if (profCargaSum == null) {
+					professorCargaHoraria.put(value.getProfessor(),
+							(int) value.getHorario().getHorarioSequencia().getValor());
+				} else {
+					professorCargaHoraria.put(value.getProfessor(),
+							profCargaSum + (int) value.getHorario().getHorarioSequencia().getValor());
+				}
+
+				if (professorCargaHoraria.get(value.getProfessor()) > value.getProfessor().getCargaHorariaSemanal()) {
+					return false;
+				}
+			}
+		}
 		return true;
 	}
 }
