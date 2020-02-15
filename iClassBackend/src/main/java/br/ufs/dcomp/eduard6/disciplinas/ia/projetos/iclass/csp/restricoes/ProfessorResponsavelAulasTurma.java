0fdb807cea24c70fd5b129702a6864651825d4ac
Eduardo Fillipe da Silva Reis <eduardo556@live.com>
2020-02-15 17:04:16 -0300

    Adicionada restrição de Professor ser Responsável por todas as aulas de uma turma.

--- iClassBackend/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/projetos/iclass/csp/restricoes/ProfessorResponsavelAulasTurma.java
+++ iClassBackend/src/main/java/br/ufs/dcomp/eduard6/disciplinas/ia/projetos/iclass/csp/restricoes/ProfessorResponsavelAulasTurma.java
@@ -6,6 +6,7 @@ import aima.core.search.csp.Assignment;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.IClassCSP;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.variables.IClassDomainRepresentation;
 import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.variables.TurmaVariable;
+import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.to.TurmaTO;
 
 /**
  * O mesmo professor deve ministrar todas as aulas de uma turma.
@@ -13,26 +14,47 @@ import br.ufs.dcomp.eduard6.disciplinas.ia.projetos.iclass.csp.variables.TurmaVa
  * Restrição opcional: O não uso dela abre espaço para que mais de um professor
  * ministre a mesma disciplina, como é o caso de Edilayne e Ricardo Pinheiro.
  * 
+ * Deve ser alterada a especificação da classe {@link TurmaTO} para comportar um
+ * array de professores, em vez de um só.
+ * 
  * @author Eduardo Fillipe da Silva Reis
  *
  */
 public class ProfessorResponsavelAulasTurma extends IClassRestricaoBase {
 
-	public ProfessorResponsavelAulasTurma(IClassCSP iClassCSP) {
+	private List<TurmaVariable> turmas;
+
+	public ProfessorResponsavelAulasTurma(IClassCSP iClassCSP, TurmaTO turma) {
 		super(iClassCSP);
-		// TODO Auto-generated constructor stub
+		this.turmas = getiClassCSP().getFragmentosTurma(turma);
 	}
 
 	@Override
 	public List<TurmaVariable> getScope() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.turmas;
 	}
 
 	@Override
 	public boolean isSatisfiedWith(Assignment<TurmaVariable, IClassDomainRepresentation> assignment) {
-		// TODO Auto-generated method stub
-		return false;
+		int primeiroNaoNull;
+		for (primeiroNaoNull = 0; primeiroNaoNull < getScope().size(); primeiroNaoNull++){
+			IClassDomainRepresentation temp = assignment.getValue(getScope().get(primeiroNaoNull));				
+			if (temp != null)
+				break;
+		}
+		
+		if (primeiroNaoNull >= getScope().size() - 1)
+			return true;
+		
+		IClassDomainRepresentation temp = assignment.getValue(getScope().get(primeiroNaoNull));				
+		for (int i = primeiroNaoNull; i < getScope().size() - 1; i++) {
+			IClassDomainRepresentation v = assignment.getValue(getScope().get(i));
+			if (v != null) {
+				if (v.getProfessor().equals(temp.getProfessor())) {
+					return false;
+				}
+			} 
+		}
+		return true;
 	}
-
 }
